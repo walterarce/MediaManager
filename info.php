eed812391aa88e8f909bc7f022b28fd1772eb48c
Walter Arce <walter.arce@inworx.com>
2021-05-26 23:28:36 -0300

    se agrega conexion y manejo de fallo

--- info.php
+++ info.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+$nombre_usuario ="warce";
+$clave ="thor2021";
+$base_datos ="socialnet";
+$servidor = "localhost";
+
+$conn = mysqli_connect($servidor,$nombre_usuario,$clave,$base_datos);
+if ($conn-> connect_error)
+{
+    die("Fallo". mysqli_error($conn));
+}
+echo "Exito";
