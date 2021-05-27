838b50ec7bebb4f3339add4c2b08c6abae051c07
Walter Arce <walter.arce@inworx.com>
2021-05-26 23:42:18 -0300

    nueva carpeta , defino constantes para los datos de acceso a la base

---
+++ DB/infodb.php
@@ -0,0 +1,5 @@
+<?php
+const nombre_usuario = "warce";
+const clave = "thor2021";
+const base_datos = "mediamanager";
+const servidor = "localhost";
--- info.php
+++ info.php
@@ -1,12 +1,28 @@
 <?php
-$nombre_usuario ="warce";
-$clave ="thor2021";
-$base_datos ="mediamanager";
-$servidor = "localhost";
+include ("DB/infodb.php");
 
-$conn = mysqli_connect($servidor,$nombre_usuario,$clave,$base_datos);
+$conn = mysqli_connect(servidor,nombre_usuario,clave,base_datos);
 if ($conn-> connect_error)
 {
     die("Fallo". mysqli_error($conn));
 }
 echo "Exito";
+
+//TODO: Resolver todo el CRUD
+
+if (!isset($_GET['accion'])) {
+    outputError(400);
+}
+
+$metodo = strtolower($_SERVER['REQUEST_METHOD']);
+$accion = explode('/', strtolower($_GET['accion']));
+$funcionNombre = $metodo . ucfirst($accion[0]);
+$parametros = array_slice($accion, 1);
+if (count($parametros) >0 && $metodo == 'get') {
+    $funcionNombre = $funcionNombre.'ConParametros';
+}
+if (function_exists($funcionNombre)) {
+    call_user_func_array ($funcionNombre, $parametros);
+} else {
+    outputError(400);
+}
